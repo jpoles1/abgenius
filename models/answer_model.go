3efc354463c919455ecd327d81cd603255dc8160
Jordan Poles <jpdev.noreply@gmail.com>
2020-02-22 23:26:07 -0500

    Fixing backend?

--- models/answer_model.go
+++ models/answer_model.go
@@ -6,8 +6,8 @@ import "github.com/globalsign/mgo/bson"
 type Answer struct {
 	ID           bson.ObjectId `bson:"_id" json:"_id"`
 	LearnerID    bson.ObjectId `bson:"lid" json:"lid"`
-	Learner      [][]string    `bson:"learner" json:"learner"`
-	Genius       [][]string    `bson:"genius" json:"genius"`
+	Learner      []string      `bson:"learner" json:"learner"`
+	Genius       []string      `bson:"genius" json:"genius"`
 	TimeElapsed  int           `bson:"timeElapsed" json:"timeElapsed"`
 	PeekedAtGaps bool          `bson:"peekedAtGaps" json:"peekedAtGaps"`
 	Grade        int           `bson:"grade" json:"grade"`
