c4b647960bfa95543763fbf68712893944726f2d
Jordan Poles <jpdev.noreply@gmail.com>
2020-10-24 19:41:48 -0400

    Release 1.9.0

---
+++ frontend/dist/precache-manifest.8ada1e42534cc6b6cbae880dd2eb1458.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "a602a4c7fca07b42b7d0",
+    "url": "/js/about.e05fcc14.js"
+  },
+  {
+    "revision": "3fb6891247bc816813ed",
+    "url": "/css/app.8ddff4ab.css"
+  },
+  {
+    "revision": "3fb6891247bc816813ed",
+    "url": "/js/app.84c28ae1.js"
+  },
+  {
+    "revision": "67c88e18d7a067613e2b",
+    "url": "/css/chunk-vendors.b90b5b7f.css"
+  },
+  {
+    "revision": "67c88e18d7a067613e2b",
+    "url": "/js/chunk-vendors.2e4a85b1.js"
+  },
+  {
+    "revision": "ebba69505a57f3fa47cb98651f44cb9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8973273de9433bb7ca6e6573a553c8f6",
+    "url": "/img/acid-base-buffer.svg"
+  },
+  {
+    "revision": "f039c6e6846dbad3d540b28e4a054f4d",
+    "url": "/img/bicarb_nh4_gen.svg"
+  },
+  {
+    "revision": "72ce0907b403404df65b7f057c230b0a",
+    "url": "/img/icon.png"
+  },
+  {
+    "revision": "946c64e720b5276e86ac95c445a9b191",
+    "url": "/img/icon.svg"
+  },
+  {
+    "revision": "696c081ae56a5d4674d5da24e7810ab7",
+    "url": "/img/google_login.png"
+  },
+  {
+    "revision": "e337858dd85e6d02632d6a2ac7aae26d",
+    "url": "/img/davenport_axis.png"
+  },
+  {
+    "revision": "fa7d1ccba2a1a2747243a9fb621888bf",
+    "url": "/img/co2_to_bicarb.png"
+  },
+  {
+    "revision": "f1451af49e43cde5fa2b78572f57f987",
+    "url": "/img/lungs.svg"
+  },
+  {
+    "revision": "d02b3281f12a2547d424dfe35412aa14",
+    "url": "/img/lungs_clear.svg"
+  },
+  {
+    "revision": "40ff965dd091ea80ce2f69bd83be0959",
+    "url": "/img/davenport.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f30aafb09a0178d486dd2d2a5fdfe847",
+    "url": "/img/renal_acid_secretion.svg"
+  },
+  {
+    "revision": "7bd589215ba03ebe054418298398e8ab",
+    "url": "/img/winters_formula_plot.png"
+  },
+  {
+    "revision": "8f7511353660bc0b45b7c97dbc4e4d6f",
+    "url": "/img/social.png"
+  }
+];
--- frontend/dist/service-worker.js
+++ frontend/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e03462f518dfd478c112974bb0bbbf63.js"
+  "/precache-manifest.8ada1e42534cc6b6cbae880dd2eb1458.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "abgenius"});
